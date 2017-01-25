9bdc41905a59e203d82d4c3bf027c5effcd9d0e5
Latasha Simmons <lsimmons1832@gmail.com>
2017-01-24 20:49:34 -0600

    arrays

--- day2.js
+++ day2.js
@@ -1,13 +1,65 @@
 //switch statements
 
-var day = "Friday";
-switch(day){
-	case "Monday":
-		console.log("Not again!");
-		break;
-	case "Friday":
-		console.log("PARTAY!!!!");
-		break;
-	default:
-		console.log("Not Monday");
-}
+//var day = "Friday";
+//switch(day){
+//	case "Monday":
+//		console.log("Not again!");
+//		break;
+//	case "Friday":
+//		console.log("PARTAY!!!!");
+//		break;
+//	default:
+//		console.log("Not Monday");
+//}
+
+//Array
+
+var bucket = ['cats', 12, 33];
+
+//add something to the end of the array
+bucket.push
+//remove something from the back of the array
+bucket.pop
+//add something to the front of the array
+bucket.shift
+//remove something from the front of the array
+bucket.unshift
+
+var fruits = ["cherries","apples","bananas"];
+var fruitString = fruit.join(", ");
+console.log("fruitString", fruitString);
+
+
+var catColors = 'green,red,yellow,black';
+var catColorsArray = catColors.split(",");
+
+var reverseFruit = fruit.reverse();
+
+var alphaFruit = fruit.sort();
+
+var nums = [9,6,8,2,4,11,66]; //2,4,6,8,9,11,66
+var numSort = nums.sort(
+	function(a,b){
+		return a-b;
+	}
+); // in order to sort numbers you need the function and return
+
+console.log("numSort", numSort);
+
+var mixedCrap = ['r','m',33,9,3];
+console.log("????",mixedCrap.sort());
+
+var animals = ['cats','turtles','fish','bears','tigers'];
+console.log("fish index", animals.indexOf("fish"));
+
+var animals2 = animals.slice(2,4);
+console.log("animals2",animals2);
+
+animals.splice(1,1);
+console.log("no turtles", animals);
+
+var colors = ["red","blue","gold","pink","orange"];
+
+//sort, reverse, make a string
+var finalAnswer = colors.sort().reverse().join(" cat ");
+console.log("finalAnswer", finalAnswer);
