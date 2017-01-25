f8cb170dc9706dae574927a0442c72263c1002cc
Latasha Simmons <lsimmons1832@gmail.com>
2017-01-24 19:22:07 -0600

    added new file

---
+++ day2.js
@@ -0,0 +1,13 @@
+//switch statements
+
+var day = "Friday";
+switch(day){
+	case "Monday":
+		console.log("Not again!");
+		break;
+	case "Friday":
+		console.log("PARTAY!!!!");
+		break;
+	default:
+		console.log("Not Monday");
+}
