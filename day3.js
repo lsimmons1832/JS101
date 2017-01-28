df8c0bced15e5a1fa3372cb4531fa145d49c9f7c
Latasha Simmons <lsimmons1832@gmail.com>
2017-01-28 11:41:13 -0600

    save work

--- day3.js
+++ day3.js
@@ -16,24 +16,33 @@ for (var myKey in myCat){
 }
 
 //Functions
+var nuggetElement = document.getElementById("nuggetShack");
+console.log("nuggetElement", nuggetElement);
 
-// function nuggetMaker(animal) {
-// 	var myNugget = animal / 6; //At this point myNugget is stuck in this function
-// 	return myNugget; //to get myNugget out you have to return it
-// }
-
-// var chickenNuggets = nuggetMaker("chicken"); //The idea is that you feed something 
-// var lobsterNuggets = nuggetMaker("lobster"); //into a function and get stuff out of it
-// var ____ = nuggetMaker("myComputer");
-
-//*****new function*****//
-function myLooper(myArray){
-	for (var i = 0; i < myArray.length; i++) {
-		console.log(myArray[i]);
-	}
+function nuggetMaker(animal) {
+	var myNugget = "breading" + animal + "breading"; //At this point myNugget is stuck in this function
+	return myNugget; //to get myNugget out you have to return it
 }
 
-var babyNames = ["Jasmine","Marie","Frances","Katia","Paula"];
-var babyShowerColors = ["pink","purple","lavender","teal","flax"]
-myLooper(babyNames);
-myLooper(babyShowerColors);
+var chickenNuggets = nuggetMaker("chicken"); //The idea is that you feed something
+console.log("chickenNuggets", chickenNuggets);
+nuggetElement.innerHTML = chickenNuggets;
+
+var lobsterNuggets = nuggetMaker("lobster"); //into a function and get stuff out of it
+console.log("lobsterNuggets",lobsterNuggets);
+nuggetElement.innerHTML = lobsterNuggets;
+
+
+//var ____ = nuggetMaker("myComputer");
+
+//*****new function*****//
+// function myLooper(myArray){
+// 	for (var i = 0; i < myArray.length; i++) {
+// 		console.log(myArray[i]);
+// 	}
+// }
+
+// var babyNames = ["Jasmine","Marie","Frances","Katia","Paula"];
+// var babyShowerColors = ["pink","purple","lavender","teal","flax"]
+// myLooper(babyNames);
+// myLooper(babyShowerColors);
