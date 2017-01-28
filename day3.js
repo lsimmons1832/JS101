c37fe7cd34ccfd6326f78ff1915fd0c608784f33
Latasha Simmons <lsimmons1832@gmail.com>
2017-01-28 11:22:04 -0600

    save work

--- day3.js
+++ day3.js
@@ -14,3 +14,26 @@ for (var myKey in myCat){
 	console.log("myKey", myKey); //Get Keys if you don't know what they are
 	console.log("values", myCat[myKey]); //Get values now that you know the keys
 }
+
+//Functions
+
+// function nuggetMaker(animal) {
+// 	var myNugget = animal / 6; //At this point myNugget is stuck in this function
+// 	return myNugget; //to get myNugget out you have to return it
+// }
+
+// var chickenNuggets = nuggetMaker("chicken"); //The idea is that you feed something 
+// var lobsterNuggets = nuggetMaker("lobster"); //into a function and get stuff out of it
+// var ____ = nuggetMaker("myComputer");
+
+//*****new function*****//
+function myLooper(myArray){
+	for (var i = 0; i < myArray.length; i++) {
+		console.log(myArray[i]);
+	}
+}
+
+var babyNames = ["Jasmine","Marie","Frances","Katia","Paula"];
+var babyShowerColors = ["pink","purple","lavender","teal","flax"]
+myLooper(babyNames);
+myLooper(babyShowerColors);
